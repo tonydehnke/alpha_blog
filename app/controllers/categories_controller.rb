4e320793db5cd9895d30f94d8b0a6663aef57ad0
Tony Dehnke <tdehnke@me.com>
2018-12-14 11:53:33 +0700

    update all view with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,8 @@ before_action :require_admin, except: [:index, :show]
   end
   
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
