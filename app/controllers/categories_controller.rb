259111f02d3cbd72648830cdd2f7166278b79e3c
Tony Dehnke <tdehnke@me.com>
2018-12-14 14:19:35 +0700

    add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,19 @@ before_action :require_admin, except: [:index, :show]
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfuly updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
   private
     
     def category_params
